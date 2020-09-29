167a7d6cea6636d15051dee0af5399194bc3703f
ines-chami <ineschami@hotmail.com>
2020-09-29 03:40:43 -0700

    update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source hyphc_env/bin/activate
 export HHC_HOME=$(pwd)
-export DATAPATH="$HHC_HOME/data"
-export SAVEPATH="$HHC_HOME/embeddings"
+export DATAPATH="$HHC_HOME/data"                # Path where to save the data files
+export SAVEPATH="$HHC_HOME/embeddings"          # Path where to save the trained models 
